912c2017743b4bb7208fac129dade0ac0d3e4a85
DESKTOP-ACU7AP4\ralfs <r.suerig@crayon.games>
2024-12-09 20:02:20 +0100

    Main

---
+++ app/src/main/java/com/plcoding/audiorecorder/record/AndroidAudioRecorder.kt
@@ -0,0 +1,42 @@
+package com.plcoding.audiorecorder.record
+
+import android.content.Context
+import android.media.MediaRecorder
+import android.os.Build
+import android.provider.MediaStore.Audio.Media
+import java.io.File
+import java.io.FileOutputStream
+
+class AndroidAudioRecorder(
+    private val context: Context
+): AudioRecorder {
+
+    private var recorder: MediaRecorder? = null
+
+    private fun createRecorder(): MediaRecorder {
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(context)
+        } else MediaRecorder()
+    }
+
+    override fun start(outputFile: File) {
+        createRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile(outputFile.absolutePath)
+
+
+            prepare()
+            start()
+
+            recorder = this
+        }
+    }
+
+    override fun stop() {
+        recorder?.stop()
+        recorder?.reset()
+        recorder = null
+    }
+}
